af75707d2438c70978b400cd787cb9e38d39e527
konamimi01 <dev.konamimi@gmail.com>
2025-03-16 17:19:59 +0900

    deploy SPA

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Error from "./Pages/Error";
+
+const homeUrl = process.env.PUBLIC_URL;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path={homeUrl} element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/Error.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Error() {
+    return (
+        <>
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        ERROR
+                    </p>
+                </header>
+            </div>
+        </>        
+    )
+}
+
+export default Error;
